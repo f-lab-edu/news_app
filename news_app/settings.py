4a83f7436d2381f47a4145b9bda5a3fc74c32dd4
HyeongDo <gudeh8080>
2023-01-01 20:38:32 +0900

    err fix

--- news_app/settings.py
+++ news_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-l#069+fvqw_a^&d=p&mt)p&74$%dj7=p)!(r)+kp_lngnq!s@)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1']
 
 
 # Application definition
